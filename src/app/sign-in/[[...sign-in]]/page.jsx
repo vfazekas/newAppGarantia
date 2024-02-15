ce6c95a3a6d929704d9ec454b836d6d4aba54d3e
vfazekas <140442311+vfazekas@users.noreply.github.com>
2024-02-15 14:16:58 -0300

    Delete src/app directory

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
- 
-export default function Page() {
-  return (
-  <div className="flex w-full h-full items-center justify-center">
-    <SignIn />
-  </div>
-  )
-}
